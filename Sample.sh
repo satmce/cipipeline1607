c860b632388ad91b371425e32fb7d9862dc2a5c9
satmce <satmce@gmail.com>
2019-07-16 16:34:56 +0530

    Update Sample.sh

--- Sample.sh
+++ Sample.sh
@@ -1,6 +1,4 @@
-from ubuntu
-label maintainer satmce@gmail.com
-env iteration_release="1.0"
-add myShell.sh /code/Sample.sh
-run chmod +x /code/Sample.sh
-cmd sh /code/Sample.sh
+#!/bin/bash
+echo "hello"
+echo "hello world!"
+ps ax
